909a21ee20cf3434d89b320e446ad3a2ccc1d203
JuanGmz <juanalfredohd@gmail.com>
2025-04-27 22:17:23 -0600

    first commit

---
+++ src/main/java/com/example/demo/models/Usuario.java
@@ -0,0 +1,51 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+
+    private String nombre;
+    private String email;
+    private Date fechaRegistro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+}
